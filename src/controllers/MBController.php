7b1d36005e8dfc42f43cbbf828865d02f0f754b6
KlarDak <mk_daking@mail.ru>
2022-04-07 14:28:08 +0300

    MyBlog 0.1.2.1 Dev
    - Добавил в роутер ссылку на тестовую страницу.

--- src/controllers/MBController.php
+++ src/controllers/MBController.php
@@ -8,13 +8,16 @@ class MBController
 {
     public static function setRoute(array $args)
     {
-        if ($args["addr"] == null)
-            return false;
+        if ($args == null)
+            MBView::sendError(404);
 
         switch ($args["addr"])
         {
             case "post":
                 MBPost::getPost($args["id"]);
+                break;
+            case "test":
+                include_once 'src/views/templates/test_page.php';
                 break;
             case 404:
                 MBView::sendError(404);
